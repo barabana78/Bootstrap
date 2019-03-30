db60cfd9ef0480fa7c05b090a12d5f539f7b5249
AndruB <barabana78@gmail.com>
2019-03-30 13:10:07 +0200

    dune

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 'use strict';
+$(document).ready(function(){
+    $('.mySlider').slick({
+        dots: false,
+        slide: 'div',
+        infinite: true,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '.row-prev',
+        nextArrow: '.row-next',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+// ПЛАВНЫЙ СКРОЛЛ
+    $("#menu, #mobileMenu, #scroll").on("click","a", function (event) {
+//отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+//забираем идентификатор блока с атрибута href
+        var id  = $(this).attr('href'),
+//узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+//анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    })
+});
